10b6bdd1947e8e1b9e0468260ce676fbad83ab6d
Aung Chan Myae <154319632+chanmyae97@users.noreply.github.com>
2024-12-14 16:44:38 +0700

    todolist demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Counter from "./assets/components/Counter";
+import Counter from "./assets/components/Counter/Counter";
+import Todo from "./assets/components/Todo/Todo";
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      {/* <Counter /> */}
+      <Todo />
     </div>
   );
 };
--- src/assets/components/Counter/Counter.jsx
+++ src/assets/components/Counter/Counter.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../style.css";
+import "./style.css";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
