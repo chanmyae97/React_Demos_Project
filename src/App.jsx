ce228a4e71a80ddd9d7b59ef30a7033d7130ca6b
Aung Chan Myae <154319632+chanmyae97@users.noreply.github.com>
2024-12-14 14:06:35 +0700

    Starting to practice demos project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./assets/components/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/components/Counter.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "../style.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <div className="container">
+      <div>
+        <h1 className="number">{count}</h1>
+      </div>
+      <div className="btns-container">
+        <button onClick={increment} className="increment">
+          +
+        </button>
+        <button onClick={decrement} className="increment">
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
